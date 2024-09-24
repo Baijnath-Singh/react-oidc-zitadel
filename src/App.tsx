3b4f104da31ad26e00f0bf672d5ab848a7398eff
baisingh <baisingh@extremenetworks.com>
2024-09-24 18:42:04 +0530

    Initial commit with project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createZitadelAuth, ZitadelConfig } from "@zitadel/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from "./components/Login";
+import Callback from "./components/Callback";
 
 function App() {
+  const config: ZitadelConfig = {
+    authority: "https://ng-iam-1-cjq5xf.us1.zitadel.cloud",
+    client_id: "286199767288075328",
+  };
+
+  const zitadel = createZitadelAuth(config);
+
+  function login() {
+    zitadel.authorize();
+  }
+
+  function signout() {
+    zitadel.signout();
+  }
+
+  const [authenticated, setAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    zitadel.userManager.getUser().then((user) => {
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+    });
+  }, [zitadel]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to ZITADEL React</p>
+
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Login authenticated={authenticated} handleLogin={login} />
+              }
+            />
+            <Route
+              path="/callback"
+              element={
+                <Callback
+                  authenticated={authenticated}
+                  setAuth={setAuthenticated}
+                  handleLogout={signout}
+                  userManager={zitadel.userManager}
+                />
+              }
+            />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
